0ef995425003bd69e0af98df2add1b1ea9391bd2
FelixMillon <felix.millon@outlook.com>
2025-03-03 12:25:12 +0100

    call header from shell (non fonctionel)

--- shell/src/App.js
+++ shell/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+
+const RemoteHeader = React.lazy(() => import('header'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suspense fallback={<div>Chargement du header...</div>}>
+        <RemoteHeader />
+      </Suspense>
+      <h2>Je suis le Shell 🚀</h2>
     </div>
   );
 }
