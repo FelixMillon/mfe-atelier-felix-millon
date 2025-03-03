dedd7d2cbb8ec97c00bfa3f34bf3f9fe77afd911
FelixMillon <felix.millon@outlook.com>
2025-03-03 12:05:53 +0100

    modification du port et affichage du header au lancement de l'application

---
+++ header/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header style={{ padding: '20px', backgroundColor: '#333', color: '#fff' }}>
+    <h1>Je suis le Header distant 🎉</h1>
+  </header>
+);
+
+export default Header;
